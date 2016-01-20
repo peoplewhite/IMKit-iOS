1ad943f9c77ead79725153956b36576613d451ce
vivalalova <vivalalova0@gmail.com>
2016-01-20 11:57:15 +0800

    add header

---
+++ IMKitSDK.framework/Headers/IMBadge.h
@@ -0,0 +1,14 @@
+//
+//  IMBadge.h
+//  IMKitSDK
+//
+//  Created by Kuo-HsunShih on 2016/1/20.
+//  Copyright © 2016年 Funtek. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface IMBadge : JSONModel
+@property (assign, nonatomic) int total;
+@property (strong, nonatomic) NSDictionary* badgeDict;
+@end
