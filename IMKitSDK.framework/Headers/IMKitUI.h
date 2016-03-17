ab9817fd3919b550187566d96ec9694b9d15a6b6
vivalalova <vivalalova0@gmail.com>
2016-03-17 19:23:32 +0800

    31

---
+++ IMKitSDK.framework/Headers/IMKitUI.h
@@ -0,0 +1,17 @@
+//
+//  IMKitUI.h
+//  RichSDKDemo
+//
+//  Created by Kuo-HsunShih on 2016/3/15.
+//  Copyright © 2016年 funtek. All rights reserved.
+//
+
+#ifndef IMKitUI_h
+#define IMKitUI_h
+
+#import "IMViewController.h"
+#import "IMRoomListViewController.h"
+#import "IMChatViewController.h"
+
+
+#endif /* IMKitUI_h */
